59b77d338b27b501fc5a411da66e22683cd2ed26
victan78 <116087124+Victan78@users.noreply.github.com>
2023-05-11 17:33:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-<?php
-session_start();
 
-$HOST = 'aws.connect.psdb.cloud';
-$USERNAME = '7tju9dd621h3sup78k4h';
-$PASSWORD = 'pscale_pw_MUmvKr1L8Se76ooq5nH2DruwwgCV0aRYlrSguhA9RpI';
-$DATABASE = 'classlink_authentification';
-$method = filter_input(INPUT_SERVER,'REQUEST_METHOD');
-$dsn = "mysql:host={$HOST};dbname={$DATABASE}";
-$options = array(
-PDO::MYSQL_ATTR_SSL_CA => "./cacert-2023-01-10.pem",
-);
-$pdo = new PDO($dsn, $USERNAME, $PASSWORD, $options);
-
-if($method == 'POST'){
-    $username = trim(filter_input(INPUT_POST, 'username'));
-    $password = trim(filter_input(INPUT_POST, 'password'));
-    $requete = $pdo->prepare("
-            INSERT INTO users (login, password) VALUES (:login, :password)
-            ");
-            $requete->execute([
-                ":login" => $username,
-                ":password" => password_hash($password, PASSWORD_DEFAULT),
-            ]);
-}
-?>
 
 <!DOCTYPE html>
 <html lang="fr">
